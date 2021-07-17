1eef603c3fe3e66abcd2d5c1e8d56f7ac6fa78a9
Matheus Iyda Fogolin <ma.fogolin@hotmail.com>
2021-07-17 00:02:30 -0300

    primeiro commit

---
+++ ApiCatalogoJogos/Services/IJogoService.cs
@@ -0,0 +1,23 @@
+﻿using ApiCatalogoJogos.InputModel;
+using ApiCatalogoJogos.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiCatalogoJogos.Services
+{
+    public interface IJogoService : IDisposable
+    {
+        Task<List<JogoViewModel>> Obter(int pagina, int quantidade);
+
+        Task<JogoViewModel> Obter(Guid id);
+
+        Task<JogoViewModel> Inserir(JogoInputModel jogo);
+
+        Task Atualizar(Guid id, double preco);   
+
+        Task Remover(Guid id);
+        Task Atualizar(Guid idJogo, JogoInputModel jogo);
+    }
+}
